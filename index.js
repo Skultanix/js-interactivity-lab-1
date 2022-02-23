4ec4361bc3d68922c992690724c02608fb573415
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-23 15:41:24 -0700

    Step 3 complete.

--- index.js
+++ index.js
@@ -17,10 +17,22 @@ function addMovie(event) {
     movieTitle.textContent = inputField.value
     //appending movieTitle to movie list
     movie.appendChild(movieTitle)
+    //adding the delete button
+    const deleteBtn = document.createElement("button")
+    //setting content of the delete button
+    deleteBtn.textContent = `X`
+    //delete button
+    deleteBtn.addEventListener("click", deleteMovie)
+    //appending x button to movie element
+    movie.appendChild(deleteBtn)
     //finding ul element
    const listActual = document.querySelector('ul')
    //appending movie titles from the list in a displayable manner
    listActual.appendChild(movie)
+}
+//delete function
+function deleteMovie(event) {
+    event.target.parentNode.remove()
 }
 //selection of form element
 const form = document.querySelector(`form`)
